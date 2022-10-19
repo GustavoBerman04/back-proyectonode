2e96f39306ff4dcfb7decdaab96b929009694abc
[Gustavo <612110430@utsalamanca.edu.mx>
2022-10-19 12:35:11 -0500

    finalizamos bd

--- index.js
+++ index.js
@@ -13,10 +13,23 @@ app.use(bodyparser.urlencoded({
 app.use(bodyparser.json())
 
 //conexion a la base de datos
+console.log(process.env)
+const uri = `mongodb+srv://${process.env.USUARIO}:${process.env.PASSWORD}@cluster0.tm5nmcw.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() =>{
+    console.log('conectado a BD')
+}).catch( e => {
+    console.log('error: ', e)
+})
 
 //importar rutas
+const authRoutes = require('./routes/auth')
 
 //ruta del Middleware
+app.use('/api/user', authRoutes)
+
 app.get('/', (req, res) =>{
     res.json({
         estado: true,
